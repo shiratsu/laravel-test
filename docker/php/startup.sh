1a03a5c936677e47ec0c7f11e8eb55a34be5252c
shiratsu <shun_hiratsuka2002@yahoo.co.jp>
2020-05-04 23:31:18 +0900

    modify startup and run-task

--- docker/php/startup.sh
+++ docker/php/startup.sh
@@ -2,8 +2,11 @@
 chown ec2-user:ec2-user -R /home/ec2-user/public_html
 chmod 705 /home/ec2-user
 
-/sbin/service crond start
-/sbin/service httpd start
+cd /home/ec2-user/public_html/hiratsuka-test
+php artisan $1
+
+#/sbin/service crond start
+/sbin/service httpd start &
 
 # /etc/rc.d/init.d/sshd start
 
@@ -11,5 +14,3 @@ while true
 do
     sleep 5
 done
-
-php artisan $1
